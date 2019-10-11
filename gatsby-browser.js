c99dfaeb70d6d594d56c0db56ceaf7ed3a2ca33c
Nick McMillan <nick.a.mcmillan@gmail.com>
2019-10-11 16:01:24 +1100

    smooth scrolling

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,5 @@
-
-const transitionDelay = 500
+// Adjust if you need to add a delay before scrolling
+const transitionDelay = 0
 
 export const shouldUpdateScroll = ({
   routerProps: { location },
@@ -8,15 +8,20 @@ export const shouldUpdateScroll = ({
   if (location.action === 'PUSH') {
     window.setTimeout(() => {
       // console.log('scroll to top')
-      
-      window.scrollTo(0, 0)
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth' // feel free to use or not
+      })
     }, transitionDelay)
   } else {
-    const savedPosition = getSavedScrollPosition(location)
+    const savedPosition = getSavedScrollPosition(location) || [0, 0]
+    const top = savedPosition[1]
     window.setTimeout(() => {
       // console.log('scroll to saved position')
-      
-      window.scrollTo(...(savedPosition || [0, 0]))
+      window.scrollTo({
+        top,
+        behavior: 'smooth'
+      })
     }, transitionDelay)
   }
   return false
